24d50eb7f296a6ee5bc45c922ed6c5402ea76f4d
helmer barcelos <snorlaxgamer17@gmail.com>
2025-06-29 21:02:53 -0300

    Create Jlogin.java
    tela de login (protótipo)

---
+++ src/main/java/view/Jlogin.java
@@ -0,0 +1,80 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+
+public class Jlogin extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Jlogin frame = new Jlogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Jlogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(82, 11, 254, 239);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Login");
+		lblNewLabel.setBounds(84, 46, 56, 14);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(84, 74, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setBounds(84, 105, 56, 14);
+		panel.add(lblSenha);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(84, 130, 86, 20);
+		panel.add(passwordField);
+		
+		JLabel lblNewLabel_1 = new JLabel("Seja bem vindo!");
+		lblNewLabel_1.setBounds(84, 11, 111, 14);
+		panel.add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Entrar");
+		btnNewButton.setBounds(84, 170, 89, 23);
+		panel.add(btnNewButton);
+	}
+}
