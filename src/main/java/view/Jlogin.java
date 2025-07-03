4f91b68beb8de859beaa8c8684250c331a51c51f
Juliana Ribeiro <julianarpaz@gmail.com>
2025-07-03 17:18:04 -0300

    wip: adicionando navegaçao e banco mockado para testes

--- src/main/java/view/Jlogin.java
+++ src/main/java/view/Jlogin.java
@@ -1,16 +1,11 @@
 package view;
 
+import com.desapega.Desapega_System.repository.UsuarioRepository;
+
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
-import javax.swing.JTextArea;
-import javax.swing.JButton;
-import javax.swing.SwingConstants;
 
 public class Jlogin extends JFrame {
 
@@ -46,35 +41,49 @@ public class Jlogin extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(82, 11, 254, 239);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Login");
 		lblNewLabel.setBounds(84, 46, 56, 14);
 		panel.add(lblNewLabel);
-		
+
 		textField = new JTextField();
 		textField.setBounds(84, 74, 86, 20);
 		panel.add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblSenha = new JLabel("Senha");
 		lblSenha.setBounds(84, 105, 56, 14);
 		panel.add(lblSenha);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(84, 130, 86, 20);
 		panel.add(passwordField);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Seja bem vindo!");
 		lblNewLabel_1.setBounds(84, 11, 111, 14);
 		panel.add(lblNewLabel_1);
-		
+
 		JButton btnNewButton = new JButton("Entrar");
 		btnNewButton.setBounds(84, 170, 89, 23);
 		panel.add(btnNewButton);
+
+		btnNewButton.addActionListener(e -> {
+			String usuario = textField.getText();
+			String senha = new String(passwordField.getPassword());
+
+			boolean autenticado = UsuarioRepository.autenticar(usuario, senha);
+			if (autenticado) {
+				JHome home = new JHome();
+				home.setVisible(true);
+				dispose();
+			} else {
+				JOptionPane.showMessageDialog(null, "Usuário ou senha inválidos.");
+			}
+		});
 	}
 }
