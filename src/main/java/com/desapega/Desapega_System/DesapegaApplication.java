0b4823412b28cfe0065666a88e73b35d306d0fa8
Juliana Ribeiro <julianarpaz@gmail.com>
2025-07-04 11:10:57 -0300

    fix: removendo dependência do JavaFX e ajustando inicialização para Swing

--- src/main/java/com/desapega/Desapega_System/DesapegaApplication.java
+++ src/main/java/com/desapega/Desapega_System/DesapegaApplication.java
@@ -1,16 +1,12 @@
-
 package com.desapega.Desapega_System;
 
-//encontra a classe JavaFxApplication no lugar certo
-import com.desapega.Desapega_System.ui.JavaFxApplication;
-
-import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesapegaApplication {
 
 	public static void main(String[] args) {
-		Application.launch(JavaFxApplication.class, args);
+		SpringApplication.run(DesapegaApplication.class, args);
 	}
 }
